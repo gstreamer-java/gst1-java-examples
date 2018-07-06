75cb01a2c14f9b0f34a98ac0600df67794861ff9
tendwong <tendwong@hotmail.com>
2018-07-06 23:15:50 +0800

    Footage Capture, Process Video from RTSP, Process Video from MP4 (#10)
    * This is an example that makes use of AppSrc and AppSink to write captured video samples on demand. The program records footage that is 5 seconds before the time in which the recording is triggered. Thus this program can also be used for surveillance etc.
    
    * Added some helpful comments.
    
    * Added slight correction to comments. Also added buffer.dispose to clearQueue.
    
    * Corrected jna issue displayed when Gst.deinit is called by adding buffer.disown. Also added a few more comments.
    
    * Format nicely.
    
    * Added simple check for RTSP with no audio. Also added copyright header.
    
    * There was always a 2 second silence at the end of each MP4 file. Solved this issue by adding speed-preset=ultrafast at the end of x264enc.
    
    * Added new example to show how to process the video before saving it into MP4.
    
    * Added 2 new examples to show how to process the video coming from either RTSP stream or MP4 file. The old TestProcessVideo.java has been removed.

---
+++ src/main/java/org/freedesktop/gstreamer/examples/TestFootageCapture.java
@@ -0,0 +1,308 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2018 Tend Wong.
+ *
+ * Copying and distribution of this file, with or without modification,
+ * are permitted in any medium without royalty provided the copyright
+ * notice and this notice are preserved.    This file is offered as-is,
+ * without any warranty.
+ *
+ */
+package org.freedesktop.gstreamer.examples;
+
+import java.net.URI;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+import org.freedesktop.gstreamer.Bin;
+import org.freedesktop.gstreamer.Buffer;
+import org.freedesktop.gstreamer.Bus;
+import org.freedesktop.gstreamer.Caps;
+import org.freedesktop.gstreamer.FlowReturn;
+import org.freedesktop.gstreamer.Gst;
+import org.freedesktop.gstreamer.Pipeline;
+import org.freedesktop.gstreamer.Sample;
+import org.freedesktop.gstreamer.elements.AppSink;
+import org.freedesktop.gstreamer.elements.AppSrc;
+import org.freedesktop.gstreamer.elements.BaseSink;
+import org.freedesktop.gstreamer.elements.PlayBin;
+import org.freedesktop.gstreamer.event.EOSEvent;
+
+/**
+ * This example shows how to use the various gstreamer mechanisms to
+ * 1) read from an RTSP stream
+ * 2) put it into a time delaying queue so that the sample that arrives at
+ *    a sink is footage captured 5 seconds ago,
+ * 3) place the sample into an AppSink which can discard or collect it into
+ *    a Java queue
+ * 4) at the user's discretion, start reading the samples from the Java queue
+ *    using an AppSrc
+ * 5) encode the samples into an MP4 stream
+ * 6) write the stream to a file using FileSink using a filename that is based
+ *    on the current time.
+ *
+ * @author Tend Wong
+ */
+public class TestFootageCapture {
+	// Size of Java queue
+	private final static int BUFFER_SIZE = 100;
+
+	private static boolean sendData = false;
+	private static ArrayBlockingQueue<Buffer> videoQueue = new ArrayBlockingQueue<Buffer>(BUFFER_SIZE);
+	private static ArrayBlockingQueue<Buffer> audioQueue = new ArrayBlockingQueue<Buffer>(BUFFER_SIZE);
+	private static StringBuffer videoCaps = new StringBuffer();
+	private static StringBuffer audioCaps = new StringBuffer();
+	private static Semaphore gotCaps = new Semaphore(2);
+	private static Semaphore canSend = new Semaphore(2);
+	private static Semaphore gotEOSPlaybin = new Semaphore(1);
+	private static Semaphore gotEOSPipeline = new Semaphore(1);
+
+	public static void main(String args[]) throws Exception {
+		Gst.init();
+
+		System.out.println("GST finished initialization.");
+
+		Scanner s = new Scanner(System.in);
+
+		// The time delaying queue is specified below. Specify a different value or take out the queue
+		// completely for real time capture.
+		Bin videoBin = Bin.launch("queue max-size-time=10000000000 min-threshold-time=5000000000 flush-on-eos=true ! appsink name=videoAppSink",true);
+		Bin audioBin = Bin.launch("queue max-size-time=10000000000 min-threshold-time=5000000000 flush-on-eos=true ! appsink name=audioAppSink",true);
+
+		AppSink videoAppSink = (AppSink) videoBin.getElementByName("videoAppSink");
+		AppSink audioAppSink = (AppSink) audioBin.getElementByName("audioAppSink");
+		videoAppSink.set("emit-signals", true);
+		audioAppSink.set("emit-signals", true);
+
+		AppSinkListener videoAppSinkListener = new AppSinkListener(videoQueue,videoCaps, gotCaps);
+		videoAppSink.connect((AppSink.NEW_SAMPLE) videoAppSinkListener);
+		AppSinkListener audioAppSinkListener = new AppSinkListener(audioQueue,audioCaps, gotCaps);
+		audioAppSink.connect((AppSink.NEW_SAMPLE) audioAppSinkListener);
+
+		// Specify rtsp url below
+		PlayBin playbin = new PlayBin("playbin");
+		playbin.setURI(URI.create("rtsp://ip:port/uri"));
+		playbin.setVideoSink(videoBin);
+		playbin.setAudioSink(audioBin);
+
+		playbin.getBus().connect((Bus.EOS) (source) -> {
+			System.out.println("Received the EOS on the playbin!!!");
+			gotEOSPlaybin.release();
+		});
+
+		// playbin.getBus().connect((Bus.ERROR) (source, code, message) -> {
+		// System.out.println("Error Source: " + source.getName());
+		// System.out.println("Error Code: " + code);
+		// System.out.println("Error Message: " + message);
+		// });
+		// playbin.getBus().connect((Bus.MESSAGE) (bus, message) -> {
+		// System.out.println("Bus Message : " + message.getStructure());
+		// });
+
+		gotEOSPlaybin.drainPermits();
+		gotCaps.drainPermits();
+		playbin.play();
+
+		System.out.println("Processing of RTSP feed started, please wait...");
+
+		Pipeline pipeline = null;
+		AppSrc videoAppSrc = null;
+		AppSrc audioAppSrc = null;
+		AppSrcListener videoAppSrcListener = null;
+		AppSrcListener audioAppSrcListener = null;
+
+		// Get caps from original video and audio stream and copy them into
+		// the respective AppSrcs. If RTSP stream has no audio, the
+		// gotCaps.tryAcquire will timeout and audioCaps will be empty.
+		gotCaps.acquire(1);
+		gotCaps.tryAcquire(5, TimeUnit.SECONDS);
+
+		// Pipeline below encodes and writes samples to MP4 file
+		// You must ensure the following plugins are available to your gstreamer
+		// installation : x264enc, h264parse, mpegtsmux, faac, aacparse
+
+		// If your RTSP feed has no audio, a different pipeline will be used that
+		// encodes a video only MP4 file.
+		boolean hasAudio = (audioCaps.length()>0);
+		if (hasAudio) {
+			pipeline = Pipeline.launch(
+				"appsrc name=videoAppSrc "+
+				"! rawvideoparse use-sink-caps=true "+
+				"! videoconvert ! x264enc speed-preset=ultrafast ! h264parse "+
+				"! mpegtsmux name=mux "+
+				"! filesink sync=false name=filesink "+
+				"appsrc name=audioAppSrc "+
+				"! rawaudioparse use-sink-caps=true "+
+				"! audioconvert ! faac ! aacparse ! mux. "
+			);
+
+			audioAppSrc = (AppSrc) pipeline.getElementByName("audioAppSrc");
+			audioAppSrc.setCaps(new Caps(audioCaps.toString()));
+			audioAppSrc.set("emit-signals", true);
+
+			audioAppSrcListener = new AppSrcListener(audioQueue,canSend);
+			audioAppSrc.connect((AppSrc.NEED_DATA) audioAppSrcListener);
+		}
+		else {
+			System.out.println("RTSP stream has no audio.");
+
+			pipeline = Pipeline.launch(
+				"appsrc name=videoAppSrc "+
+				"! rawvideoparse use-sink-caps=true "+
+				"! videoconvert ! x264enc speed-preset=ultrafast ! h264parse "+
+				"! mpegtsmux name=mux "+
+				"! filesink sync=false name=filesink "
+			);
+		}
+
+		videoAppSrc = (AppSrc) pipeline.getElementByName("videoAppSrc");
+		videoAppSrc.setCaps(new Caps(videoCaps.toString()));
+		videoAppSrc.set("emit-signals", true);
+
+		videoAppSrcListener = new AppSrcListener(videoQueue,canSend);
+		videoAppSrc.connect((AppSrc.NEED_DATA) videoAppSrcListener);
+
+		pipeline.getBus().connect((Bus.EOS) (source) -> {
+			System.out.println("Received the EOS on the pipeline!!!");
+			gotEOSPipeline.release();
+		});
+
+		// pipeline.getBus().connect((Bus.ERROR) (source, code, message) -> {
+		// System.out.println("Error Source: " + source.getName());
+		// System.out.println("Error Code: " + code);
+		// System.out.println("Error Message: " + message);
+		// });
+		// pipeline.getBus().connect((Bus.MESSAGE) (bus, message) -> {
+		// System.out.println("Bus Message : "+message.getStructure());
+		// });
+
+		while (true) {
+			System.out.println("Press ENTER to start capturing footage from the cam 5 seconds ago, or type 'QUIT' and press ENTER to exit...");
+			if (!s.nextLine().isEmpty())
+				break;
+
+			// Specify filename of MP4 file based on current time
+			BaseSink filesink = (BaseSink) pipeline.getElementByName("filesink");
+			filesink.set("location", "capture" + System.currentTimeMillis() + ".mp4");
+
+			// Clear any unread buffers from previous capture in the Java queues
+			if (hasAudio) {
+				clearQueue(audioQueue);
+				audioAppSrcListener.resetSendFlagged();
+			}
+			clearQueue(videoQueue);
+			videoAppSrcListener.resetSendFlagged();
+
+			gotEOSPipeline.drainPermits();
+			canSend.drainPermits();
+			pipeline.play();
+
+			// Make sure that both video and audio buffers are streamed out at
+			// the same time otherwise you get video or sound first.
+			canSend.acquire(hasAudio ? 2 : 1);
+			sendData = true;
+
+			System.out.println("Press ENTER to stop the capture...");
+			s.nextLine();
+
+			pipeline.sendEvent(new EOSEvent());
+			gotEOSPipeline.acquire(1);
+			System.out.println("Capture stopped.");
+
+			pipeline.stop();
+			sendData = false;
+		}
+
+		playbin.sendEvent(new EOSEvent());
+		gotEOSPlaybin.acquire(1);
+		System.out.println("Stopped processing of RTSP feed.");
+
+		playbin.stop();
+
+		System.out.println("Exiting program.");
+
+		Gst.deinit();
+	}
+
+	private static void clearQueue(ArrayBlockingQueue<Buffer> queue) {
+		queue.clear();
+	}
+
+	private static class AppSinkListener implements AppSink.NEW_SAMPLE {
+		private ArrayBlockingQueue<Buffer> queue;
+		private StringBuffer caps;
+		private Semaphore gotCaps;
+
+		private boolean capsSet;
+
+		public AppSinkListener(ArrayBlockingQueue<Buffer> queue, StringBuffer caps, Semaphore gotCaps) {
+			this.queue = queue;
+			this.caps = caps;
+			this.gotCaps = gotCaps;
+			capsSet = false;
+		}
+
+		@Override
+		public FlowReturn newSample(AppSink elem) {
+			Sample sample = elem.pullSample();
+
+			if (!capsSet) {
+				caps.append(sample.getCaps().toString());
+				capsSet = true;
+				gotCaps.release();
+			}
+
+			// This section will be executed only when the sample needs to be
+			// passed to the src.
+			// When sendData is true, the sample's buffer will be duplicated
+			// using buffer.copy and offered to the respective queue
+			// (videoQueue or audioQueue).
+			// Buffer's copy must disown the native object held by original
+			// buffer otherwise a jna error will be issued.
+
+			if (sendData) {
+				Buffer buffer = sample.getBuffer().copy();
+				buffer.disown();
+				queue.offer(buffer);
+			}
+
+			sample.dispose();
+
+			return FlowReturn.OK;
+		}
+	}
+
+	private static class AppSrcListener implements AppSrc.NEED_DATA {
+		private ArrayBlockingQueue<Buffer> queue;
+		private Semaphore canSend;
+
+		private boolean sendFlagged;
+
+		public AppSrcListener(ArrayBlockingQueue<Buffer> queue, Semaphore canSend) {
+			this.queue = queue;
+			this.canSend = canSend;
+			sendFlagged = false;
+		}
+
+		public void resetSendFlagged() {
+			sendFlagged = false;
+		}
+
+		@Override
+		public void needData(AppSrc elem, int size) {
+			if (!sendFlagged) {
+				sendFlagged = true;
+				canSend.release();
+			}
+
+			try {
+				elem.pushBuffer(queue.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/java/org/freedesktop/gstreamer/examples/TestProcessVideoMP4ToMP4.java
@@ -0,0 +1,431 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2018 Tend Wong.
+ *
+ * Copying and distribution of this file, with or without modification,
+ * are permitted in any medium without royalty provided the copyright
+ * notice and this notice are preserved.    This file is offered as-is,
+ * without any warranty.
+ *
+ */
+ package org.freedesktop.gstreamer.examples;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.io.File;
+import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Semaphore;
+
+import org.freedesktop.gstreamer.Bin;
+import org.freedesktop.gstreamer.Buffer;
+import org.freedesktop.gstreamer.Bus;
+import org.freedesktop.gstreamer.Caps;
+import org.freedesktop.gstreamer.ClockTime;
+import org.freedesktop.gstreamer.FlowReturn;
+import org.freedesktop.gstreamer.Gst;
+import org.freedesktop.gstreamer.Pipeline;
+import org.freedesktop.gstreamer.Sample;
+import org.freedesktop.gstreamer.Structure;
+import org.freedesktop.gstreamer.elements.AppSink;
+import org.freedesktop.gstreamer.elements.AppSrc;
+import org.freedesktop.gstreamer.elements.BaseSink;
+import org.freedesktop.gstreamer.elements.PlayBin;
+import org.freedesktop.gstreamer.event.EOSEvent;
+
+/**
+ * This example shows how to use the various gstreamer mechanisms to
+ * read from an MP4 file, process it frame by frame, then write the processed
+ * frames to an MP4 file.
+ *
+ * @author Tend Wong
+ */
+public class TestProcessVideoMP4ToMP4 {
+	private static boolean sendData = false;
+	private static ArrayBlockingQueue<Buffer> videoQueue = new ArrayBlockingQueue<Buffer>(1);
+	
+	private static StringBuffer videoCaps = new StringBuffer();
+	private static Semaphore gotCaps = new Semaphore(2);
+	private static Semaphore canSend = new Semaphore(2);
+	private static Semaphore gotEOSPlaybin = new Semaphore(1);
+	private static Semaphore gotEOSPipeline = new Semaphore(1);
+	
+	private static int videoWidth;
+	private static int videoHeight;
+	private static int numPixels;
+
+	private static ArrayBlockingQueue<FrameInfo> processingQueue = new ArrayBlockingQueue<FrameInfo>(1);
+	
+	private static boolean processData = true;
+	private static ProcessingThread processingThread;
+
+	public static void main(String args[]) throws Exception {
+		Gst.init();
+		
+		System.out.println("GST finished initialization.");
+		
+		Scanner s = new Scanner(System.in);
+
+        Bin videoBin = Bin.launch("appsink name=videoAppSink", true);
+
+        AppSink videoAppSink = (AppSink) videoBin.getElementByName("videoAppSink");
+        videoAppSink.set("emit-signals", true);
+        videoAppSink.enableAsync(false);
+        
+        AppSinkListener videoAppSinkListener = new AppSinkListener(processingQueue,videoCaps,gotCaps);
+        videoAppSink.connect((AppSink.NEW_SAMPLE) videoAppSinkListener);   
+
+        StringBuilder caps = new StringBuilder("video/x-raw,pixel-aspect-ratio=1/1,");
+        if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN)
+            caps.append("format=BGRx");
+        else
+            caps.append("format=xRGB");
+        videoAppSink.setCaps(new Caps(caps.toString()));
+        
+        PlayBin playbin = new PlayBin("playbin");
+        playbin.setURI((new File("<your mp4 file path>.mp4")).toURI());
+        playbin.setVideoSink(videoBin);
+        playbin.setAudioSink(null);
+
+        playbin.getBus().connect((Bus.EOS) (source) -> {
+			System.out.println("Received the EOS on the playbin!!!");
+			gotEOSPlaybin.release();       	
+        });
+		       
+//		playbin.getBus().connect((Bus.ERROR) (source, code, message) -> {
+//			System.out.println("Error Source: " + source.getName());
+//			System.out.println("Error Code: " + code);
+//			System.out.println("Error Message: " + message);
+//		});
+//		playbin.getBus().connect((Bus.MESSAGE) (bus, message) -> {
+//			System.out.println("Bus Message : " + message.getStructure());
+//		});
+        
+        processData = true;
+        processingThread = new ProcessingThread();
+        processingThread.setDaemon(true);
+        processingThread.start();
+        
+		gotEOSPlaybin.drainPermits();
+        gotCaps.drainPermits();
+        playbin.play();
+		
+		System.out.println("Processing of MP4 started, please wait...");
+				
+		Pipeline pipeline = null;
+		AppSrc videoAppSrc = null;
+		AppSrcListener videoAppSrcListener = null;
+		
+		gotCaps.acquire(1);
+
+		pipeline = Pipeline.launch(
+			"appsrc name=videoAppSrc "+
+			"! videoconvert ! video/x-raw,format=I420 "+
+			"! x264enc ! h264parse "+
+			"! mpegtsmux name=mux "+
+			"! filesink name=filesink "
+		);			
+
+        videoAppSrc = (AppSrc) pipeline.getElementByName("videoAppSrc");
+		videoAppSrc.setCaps(new Caps(videoCaps.toString()));
+        videoAppSrc.set("emit-signals", true);
+
+        videoAppSrcListener = new AppSrcListener(videoQueue,canSend);
+        videoAppSrc.connect((AppSrc.NEED_DATA) videoAppSrcListener);   
+
+		pipeline.getBus().connect((Bus.EOS) (source) -> {
+			System.out.println("Received the EOS on the pipeline!!!");
+			gotEOSPipeline.release();       	
+        });
+        
+//		pipeline.getBus().connect((Bus.ERROR) (source, code, message) -> {
+//            System.out.println("Error Source: " + source.getName());
+//            System.out.println("Error Code: " + code);
+//            System.out.println("Error Message: " + message);			
+//		});
+//		pipeline.getBus().connect((Bus.MESSAGE) (bus, message) -> {
+//			System.out.println("Bus Message : "+message.getStructure());			
+//		});
+		
+		BaseSink filesink = (BaseSink) pipeline.getElementByName("filesink");
+		
+		while (true) {
+			System.out.println("Press ENTER to start processing footage from the MP4, or type 'QUIT' and press ENTER to exit...");
+			if (!s.nextLine().isEmpty())
+				break;
+	
+			filesink.set("location", "processed"+System.currentTimeMillis()+".mp4");
+	
+			clearQueue(videoQueue);
+			clearQueue(processingQueue);
+			videoAppSrcListener.resetSendFlagged();
+			
+			gotEOSPipeline.drainPermits();
+			canSend.drainPermits();
+			
+	        pipeline.play();
+	
+			canSend.acquire(1);
+			sendData = true;
+	
+			System.out.println("Press ENTER to stop the process...");
+			s.nextLine();
+
+			pipeline.sendEvent(new EOSEvent());
+			gotEOSPipeline.acquire(1);
+			System.out.println("Process stopped.");
+	
+			pipeline.stop();	
+			sendData = false;
+		}
+		
+		playbin.sendEvent(new EOSEvent());
+		clearQueue(processingQueue);
+		gotEOSPlaybin.acquire(1);
+		System.out.println("Stopped processing of MP4.");
+		
+		playbin.stop();
+		
+		processData = false;
+		processingThread.interrupt();
+
+		System.out.println("Exiting program.");
+
+		Gst.deinit();
+	}
+	
+	private static void clearQueue(ArrayBlockingQueue<?> queue) {
+		queue.clear();
+	}
+	
+	private static class AppSinkListener implements AppSink.NEW_SAMPLE {
+		private ArrayBlockingQueue<FrameInfo> queue;
+		private StringBuffer caps;
+		private Semaphore gotCaps;
+		
+		private boolean capsSet;
+		
+		public AppSinkListener(ArrayBlockingQueue<FrameInfo> queue,StringBuffer caps,Semaphore gotCaps) {
+			this.queue = queue;
+			this.caps = caps;
+			this.gotCaps = gotCaps;
+			capsSet = false;
+		}
+		
+		@Override
+		public FlowReturn newSample(AppSink elem) {
+            Sample sample = elem.pullSample();
+            
+            if (!capsSet) {
+            	caps.append(sample.getCaps().toString());
+            	
+                Structure capsStruct = sample.getCaps().getStructure(0);
+                videoWidth = capsStruct.getInteger("width");
+                videoHeight = capsStruct.getInteger("height");
+        		numPixels = videoWidth * videoHeight;
+
+                capsSet = true;
+            	gotCaps.release();
+            }
+
+            if (sendData) {
+            	Buffer srcBuffer = sample.getBuffer();
+            	ByteBuffer bb = srcBuffer.map(false);
+
+        		int[] pixels = new int[numPixels];
+        		bb.asIntBuffer().get(pixels, 0, numPixels);
+
+        		FrameInfo info = new FrameInfo();
+        		info.setCapacity(bb.capacity());
+        		info.setPixels(pixels);
+        		info.setFlags(srcBuffer.getFlags());
+        		info.setDuration(srcBuffer.getDuration());
+        		info.setOffset(srcBuffer.getOffset());
+        		info.setOffsetEnd(srcBuffer.getOffsetEnd());
+        		info.setDecodeTimestamp(srcBuffer.getDecodeTimestamp());
+        		info.setPresentationTimestamp(srcBuffer.getPresentationTimestamp());
+            	
+        		try {queue.put(info);} catch (Exception e) {}
+        		
+            	srcBuffer.unmap();
+            }
+
+            sample.dispose();
+            
+            return FlowReturn.OK;		
+		}
+	}
+	
+	private static class AppSrcListener implements AppSrc.NEED_DATA {
+		private ArrayBlockingQueue<Buffer> queue;
+		private Semaphore canSend;
+		
+		private boolean sendFlagged;
+		
+		public AppSrcListener(ArrayBlockingQueue<Buffer> queue,Semaphore canSend) {
+			this.queue = queue;
+			this.canSend = canSend;
+			sendFlagged = false;
+		}
+
+		public void resetSendFlagged() {
+			sendFlagged = false;
+		}
+		
+		@Override
+		public void needData(AppSrc elem, int size) {
+			if (!sendFlagged) {
+				sendFlagged = true;
+				canSend.release();
+			}
+			
+			try {
+				elem.pushBuffer(queue.take());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static class FrameInfo {
+		private int capacity;
+		private int[] pixels;
+		
+		private int flags;
+		private ClockTime duration;
+		private long offset;
+		private long offsetEnd;
+		private ClockTime decodeTimestamp;
+		private ClockTime presentationTimestamp;
+
+		public int getCapacity() {
+			return capacity;
+		}
+		public void setCapacity(int capacity) {
+			this.capacity = capacity;
+		}
+		public int[] getPixels() {
+			return pixels;
+		}
+		public void setPixels(int[] pixels) {
+			this.pixels = pixels;
+		}
+		public int getFlags() {
+			return flags;
+		}
+		public void setFlags(int flags) {
+			this.flags = flags;
+		}
+		public ClockTime getDuration() {
+			return duration;
+		}
+		public void setDuration(ClockTime duration) {
+			this.duration = duration;
+		}
+		public long getOffset() {
+			return offset;
+		}
+		public void setOffset(long offset) {
+			this.offset = offset;
+		}
+		public long getOffsetEnd() {
+			return offsetEnd;
+		}
+		public void setOffsetEnd(long offsetEnd) {
+			this.offsetEnd = offsetEnd;
+		}
+		public ClockTime getDecodeTimestamp() {
+			return decodeTimestamp;
+		}
+		public void setDecodeTimestamp(ClockTime decodeTimestamp) {
+			this.decodeTimestamp = decodeTimestamp;
+		}
+		public ClockTime getPresentationTimestamp() {
+			return presentationTimestamp;
+		}
+		public void setPresentationTimestamp(ClockTime presentationTimestamp) {
+			this.presentationTimestamp = presentationTimestamp;
+		}	
+	}
+	
+    private static class ProcessingThread extends Thread {
+    	BufferedImage image = null;
+        int width;
+        int height;
+    	int x;
+    	int y;
+    	int dx;
+    	int dy;
+		  	
+		@Override
+		public void run() {
+			while (processData) {
+				try {
+					FrameInfo info = processingQueue.take();
+					
+					// Perform processing here, currently all this does
+					// is bounce a rectangle around the edges of the video.
+					// Unlike processing of RTSP streams, the process here
+					// need not be quick as the MP4 upstream will wait for
+					// downstream components to finish processing.
+					if (image==null) {
+						image = new BufferedImage(videoWidth, videoHeight, BufferedImage.TYPE_INT_RGB);
+						image.setAccelerationPriority(0.0f);
+						
+			            width = videoWidth/2;
+			            height = videoHeight/2;
+			            x = width/2;
+			            y = height/2;
+			            dx = Math.random()<0.5 ? -1 : 1;
+			            dy = Math.random()<0.5 ? -1 : 1;
+					}
+					
+		            int[] destPixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+		            System.arraycopy(info.getPixels(), 0, destPixels, 0, numPixels);
+					
+	        		Graphics2D g2d = image.createGraphics();
+        			g2d.setColor(Color.RED);
+        			g2d.drawRect(x, y, width, height);
+            		g2d.dispose();            
+            		
+            		if (((x+width)==videoWidth)||(x==0))
+            			dx = -dx;
+            		if (((y+height)==videoHeight)||(y==0))
+            			dy = -dy;
+            		x += dx;
+            		y += dy;
+					// End of processing
+					
+	            	Buffer dstBuffer = new Buffer(info.getCapacity());	            	
+	            	dstBuffer.map(true).asIntBuffer().put(destPixels);
+	            	dstBuffer.unmap(); 
+	            	
+	            	dstBuffer.setFlags(info.getFlags());
+	            	dstBuffer.setDuration(info.getDuration());
+	            	dstBuffer.setOffset(info.getOffset());
+	            	dstBuffer.setOffsetEnd(info.getOffsetEnd());
+	            	dstBuffer.setDecodeTimestamp(info.getDecodeTimestamp());
+	            	dstBuffer.setPresentationTimestamp(info.getPresentationTimestamp());
+					
+	            	dstBuffer.disown();
+	        		
+	        		videoQueue.put(dstBuffer);
+				}
+				catch (InterruptedException i) {
+					break;
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+    }
+
+}
